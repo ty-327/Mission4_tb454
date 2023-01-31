3fe05993f37e266376513eb898aabbd845549e6c
ty-327 <tyisb7@gmail.com>
2023-01-30 20:39:50 -0700

    Add project files.

---
+++ Mission4_tb454/wwwroot/js/gradeCalculator.js
@@ -0,0 +1,13 @@
+﻿$("#calculateBtn").click(function () {
+    assignmentScore = $("#assignmentScore").val();
+    groupProjScore = $("#groupProjScore").val();
+    quizScore = $("#quizScore").val();
+    midtermScore = $("#midtermScore").val();
+    finalExamScore = $("#finalExamScore").val();
+    intexScore = $("#intexScore").val();
+
+    finalScore = (assignmentScore * .5) + (groupProjScore * .1) + (quizScore * .1) +
+        (midtermScore * .1) + (finalExamScore * .1) + (intexScore * .1)
+
+    alert("Final Grade: " + (Math.round(finalScore * 100) / 100) + "%");
+})
