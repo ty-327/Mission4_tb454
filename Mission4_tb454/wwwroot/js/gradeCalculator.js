633d82cae560fed7c0bdd64819bd8f69b24cd0d9
ty-327 <tyisb7@gmail.com>
2023-01-30 21:09:24 -0700

    first and only commit i hope lol

--- Mission4_tb454/wwwroot/js/gradeCalculator.js
+++ Mission4_tb454/wwwroot/js/gradeCalculator.js
@@ -7,7 +7,46 @@
     intexScore = $("#intexScore").val();
 
     finalScore = (assignmentScore * .5) + (groupProjScore * .1) + (quizScore * .1) +
-        (midtermScore * .1) + (finalExamScore * .1) + (intexScore * .1)
+        (midtermScore * .1) + (finalExamScore * .1) + (intexScore * .1);
 
-    alert("Final Grade: " + (Math.round(finalScore * 100) / 100) + "%");
+    if (finalScore >= 94.00) {
+        finalLetterGrade = "A";
+    }
+    else if (finalScore >= 90.00) {
+        finalLetterGrade = "A-";
+    }
+    else if (finalScore >= 87) {
+        finalLetterGrade = "B+";
+    }
+    else if (finalScore >= 84) {
+        finalLetterGrade = "B";
+    }
+    else if (finalScore >= 80) {
+        finalLetterGrade = "B-";
+    }
+    else if (finalScore >= 77) {
+        finalLetterGrade = "C+";
+    }
+    else if (finalScore >= 74) {
+        finalLetterGrade = "C";
+    }
+    else if (finalScore >= 70) {
+        finalLetterGrade = "C-";
+    }
+    else if (finalScore >= 67) {
+        finalLetterGrade = "D+";
+    }
+    else if (finalScore >= 64) {
+        finalLetterGrade = "D";
+    }
+    else if (finalScore >= 60) {
+        finalLetterGrade = "D-";
+    }
+    else if (finalScore < 60) {
+        finalLetterGrade = "E";
+    }
+
+    $(".finalGradeP .finalGradeSpan").html((
+        Math.round(finalScore * 100) / 100) + "% / " + finalLetterGrade
+    );
 })
