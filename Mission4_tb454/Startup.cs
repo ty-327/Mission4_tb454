359728cbfb64b42d93a4802f81a1db2725b26793
ty-327 <tyisb7@gmail.com>
2023-02-01 13:31:10 -0700

    creating default template razor files

--- Mission4_tb454/Startup.cs
+++ Mission4_tb454/Startup.cs
@@ -22,6 +22,10 @@ namespace Mission4_tb454
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsEnvironment("Development")) {
+                app.UseDeveloperExceptionPage();
+            }
+            
             app.UseStaticFiles();
 
             app.UseRouting();
